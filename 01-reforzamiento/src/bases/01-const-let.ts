4edcbfb9a013f40f69aaf13e7d55a938cd7820f6
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-06-22 16:40:06 -0400

    feat: variables y constantes

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+let firstName = 'Ignacio';
+
+// Podemos asignarle un itpo de dato a una const
+const lastName: string = 'Ballegrande';
+
+console.log(firstName, lastName);
+
+let containsLetter = lastName.includes('B');
+
+// Para mostrar la variable y el valor de la variable usamos {}
+console.log({containsLetter});
--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -1,5 +1,7 @@
 import './style.css'
 
+import './bases/01-const-let';
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <section id="center">
   <h1>Hola Mundo!</h1>

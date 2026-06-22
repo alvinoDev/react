17d677d1fae3c259f8115b50631fb4d5b417bd04
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-06-22 19:04:36 -0400

    feat: Funciones con múltiples retornos 2

--- 01-reforzamiento/src/bases/06-functions.ts
+++ 01-reforzamiento/src/bases/06-functions.ts
@@ -2,29 +2,50 @@ function greet(name: string): string {
   return `Hola ${name}`;
 }
 
-const greetDos = (name: string) => {
-  return `Hola ${name}`;
-}
+const greetDos = (name: string) =>  `Hola ${name}`;
 
 const message = greet('Goku');
 const messageDos = greetDos('Vegeta');
 
 console.log(message, messageDos);
 
-function getUser() {
+interface User {
+  uid: string;
+  username: string;
+}
+
+
+function getUser(): User {
   return {
     uid: 'ABC-123',
     username: 'El_Gato',
   }
 }
 
-const getUserDos = () => {
-  return {
+const getUserDos = (): User => ({
     uid: 'DEF-123',
     username: 'El_Perro',
-  }
-}
+  })
 
 const user = getUser()
 const user2 = getUserDos()
 console.log({user, user2});
+
+
+const myNumbers: number[] = [2, 3, 4, 5, 6];
+
+// myNumbers.forEach(function (value) {
+//   console.log({ value });
+// });
+
+myNumbers.forEach( (value) => {
+  console.log({ value });
+});
+
+
+myNumbers.forEach( (value, index, arr) => {
+  console.log(value, index, arr);
+});
+
+// Forma corta de hacer lo anterior
+// myNumbers.forEach( console.log );

8cdd596c07cd1841090d4b10deb2abf6e79391d3
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-06-23 12:38:16 -0400

    feat: Interfaces y Enumeraciones

---
+++ 01-reforzamiento/src/data/heroes.data.ts
@@ -0,0 +1,46 @@
+interface Hero {
+  id: number;
+  name: string;
+  owner: Owner;
+}
+
+// Creamos los types para cuando tenemos valores especificos
+// type Owner = 'DC' | 'Marvel';
+
+// enum Owner {
+//   DC,// 0
+//   Marvel,// 1
+// }
+
+enum Owner {
+  DC = "DC", // 'DC;
+  Marvel = "Marvel", // 'Marvel'
+};
+
+const heroes: Hero[] = [
+  {
+    id: 1,
+    name: "Batman",
+    owner: Owner.DC,
+  },
+  {
+    id: 2,
+    name: "Spiderman",
+    owner: Owner.Marvel,
+  },
+  {
+    id: 3,
+    name: "Superman",
+    owner: Owner.DC,
+  },
+  {
+    id: 4,
+    name: "Flash",
+    owner: Owner.DC,
+  },
+  {
+    id: 5,
+    name: "Wolverine",
+    owner: Owner.Marvel,
+  },
+];

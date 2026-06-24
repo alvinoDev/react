3057b2acd111f37ebdf4357cf9eff6c80a3bab62
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-06-24 11:53:27 -0400

    feat: Mi primer componente

---
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -0,0 +1,14 @@
+export function FirstStepsApp() {
+  return (
+    <>
+      <h1>HELLO WORLD!</h1>
+      <p>Example paragraph!!</p>
+  
+      <button>Click me!</button>
+  
+      <div>
+        <h2>This element is inside a div</h2>
+      </div>
+    </>
+  );
+}

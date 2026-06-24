647b382fbc2bdd515d65af1d3d825df750ba1f25
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-06-24 12:11:51 -0400

    feat: Impresión de variables

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,3 +1,15 @@
+const firstName = "Arturo";
+const lastName = "de Bretaña";
+
+const favoriteGames = ["Elden Ring", "Smash", "Pokemon"];
+
+const isActive = true;
+
+const address = {
+  zipCode: "321-ASD",
+  country: "Bretaña",
+};
+
 // Forma Tradicional
 // export function MyAwesomeApp() {
 //   return (
@@ -12,8 +24,15 @@
 export const MyAwesomeApp = () => {
   return (
     <>
-      <h1>Arturo</h1>
-      <h3>de Bretaña</h3>
+      <h1>{firstName}</h1>
+      <h3>{lastName}</h3>
+
+      <p>{favoriteGames.join(", ")}</p>
+      <p>{2 + 7}</p>
+
+      <h2>{isActive ? "Activo" : "No Activo"}</h2>
+
+      <p>{JSON.stringify(address)}</p>
     </>
-  )
-} 
+  );
+};

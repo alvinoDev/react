b1bd9c9772c430fd26f2691e8723b053b9364ccf
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-06-24 12:00:18 -0400

    feat: Ejercicio con segundo componente

---
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -0,0 +1,19 @@
+// Forma Tradicional
+// export function MyAwesomeApp() {
+//   return (
+//     <>
+//       <h1>Arturo</h1>
+//       <h3>de Bretaña</h3>
+//     </>
+//   )
+// }
+
+// Forma Moderna
+export const MyAwesomeApp = () => {
+  return (
+    <>
+      <h1>Arturo</h1>
+      <h3>de Bretaña</h3>
+    </>
+  )
+} 

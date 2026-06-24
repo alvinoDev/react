3bea1808fd1a1099af6ef5d40c74fc6313d827c9
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-06-23 22:15:37 -0400

    feat: Fetch API e Interfaces y optimización con giphy

--- 01-reforzamiento/src/bases/12-fetch-api.ts
+++ 01-reforzamiento/src/bases/12-fetch-api.ts
@@ -1,19 +1,26 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
+
 const API_KEY = "9QWZ8UwklsZixcj7EoR9ZFLxt8oMaHMN";
 
 const myRequest = fetch(
   `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`,
 );
 
+const createImageInsideDOM = (url: string) => {
+  const imgElement = document.createElement('img');
+  imgElement.src = url;
+  document.body.append(imgElement);
+}
 myRequest
   .then((response) => response.json())
-  .then((data) => {
-    const imgUrl = data.data.images.original.url;
-    console.log(imgUrl)
+  .then(({ data }: GiphyRandomResponse) => {
 
-    const imgElement = document.createElement('img');
-    imgElement.src = imgUrl;
-    document.body.append(imgElement);
+    const imgUrl = data.images.original.url;
     
+    // const imgUrl = data.data.images.original.url;
+    // console.log(imgUrl)
+
+    createImageInsideDOM(imgUrl);    
   })
   .catch((err) => {
     console.log(err);
